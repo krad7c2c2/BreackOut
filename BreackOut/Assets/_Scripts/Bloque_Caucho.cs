814a450e670fe68aa6d41074a3dbd7addcf08418
Carlos Alberto Orta Cruz <caorta@telstock.net>
2025-03-18 20:17:27 -0600

    Tarea Modulo 10
    Tarea del Modulo 10

---
+++ BreackOut/Assets/_Scripts/Bloque_Caucho.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bloque_Caucho : Bloque
+{
+
+    //propiedades de bloque: 
+    bool velocidadDoble = true; //retorna la pelota a una velocidad mayor. 
+    bool dobleImpacto = true; // al rebotar la pelota e impactar con otros bloques destruye todos en linea recta, siempre y cuando sean de tipo madera y cristal. 
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        resistencia = 6;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ BreackOut/Assets/_Scripts/Bloque_Cristal.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bloque_Cristal : Bloque
+{
+
+    //propiedades de bloque: 
+    bool reboteInverso = true; //no cambia la dirección de donde viene la pelota. 
+    bool fractura = true; //si la pelota pega en el centro el bloque se rompe,
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        resistencia = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
