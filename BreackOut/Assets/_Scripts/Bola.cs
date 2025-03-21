76d39979e9e48815bf1f553110d59f9030203cfa
Carlos Alberto Orta Cruz <caorta@telstock.net>
2025-03-20 19:10:02 -0600

    Movimiento

--- BreackOut/Assets/_Scripts/Bola.cs
+++ BreackOut/Assets/_Scripts/Bola.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Bola : MonoBehaviour
 {
+
+    bool isGameStarted = false;
+    [SerializeField] public float velociadBola = 10.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //coloca la bola arriba del paddle
+        Vector3 posicionInicial = GameObject.FindGameObjectWithTag("Jugador").transform.position;
+        posicionInicial.y += 3;
+        this.transform.position = posicionInicial;
+        this.transform.SetParent(GameObject.FindGameObjectWithTag("Jugador").transform);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //detecta la tecla espacio
+        if (Input.GetKey(KeyCode.Space) || Input.GetButton("Submit")) {
+            if (!isGameStarted) {
+
+                isGameStarted = true;
+                this.transform.SetParent(null);
+                GetComponent<Rigidbody>().velocity = velociadBola * Vector3.up;
+
+            }
+        }
+
     }
 }
--- BreackOut/Assets/_Scripts/Jugador.cs
+++ BreackOut/Assets/_Scripts/Jugador.cs
@@ -4,15 +4,75 @@ using UnityEngine;
 
 public class Jugador : MonoBehaviour
 {
+
+    [SerializeField] public int limiteX = 23; //
+    [SerializeField] public float velocidadPadle = 35f; //
+
+
+    Transform transform;
+    Vector3 mousePos2D;
+    Vector3 mousePos3D;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform = this.gameObject.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        //mueve con el teclado
+        if (Input.GetKey(KeyCode.RightArrow)) { //mueve a la derecha
+            transform.Translate(Vector3.down * velocidadPadle * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            transform.Translate(Vector3.up * velocidadPadle * Time.deltaTime);
+        }
+        Vector3 pos = transform.position;
+        if (pos.x < -limiteX)
+        {
+            pos.x = -limiteX;
+        }
+        else if (pos.x > limiteX)
+        {
+            pos.x = limiteX;
+        }
+        transform.position = pos; //cambia la posicion del objeto
         
+
+        //mueve con el control..
+        /*
+        transform.Translate(Input.GetAxis("Horizontal") * Vector3.down * velocidadPadle * Time.deltaTime);
+        Vector3 pos = transform.position;
+        if (pos.x < -limiteX)
+        {
+            pos.x = -limiteX;
+        }
+        else if (pos.x > limiteX)
+        {
+            pos.x = limiteX;
+        }
+        transform.position = pos; //cambia la posicion del objeto
+        */
+
+
+        /* actualizar con el mouse
+        //mueve el paddle
+         //mousePos2D = Input.mousePosition;
+        //mousePos2D.z = -Camera.main.transform.position.z;
+        //mousePos3D = Camera.main.ScreenToViewportPoint(mousePos2D); //interpolar las cordenadas al videojuego
+
+        Vector3 pos = this.transform.position; //obtiene la posiscion
+        pos.x = mousePos3D.x;  //actualiza la posicion en el eje x de la posicion del raton
+        if (pos.x < -limiteX) {
+            pos.x = -limiteX;
+        } else if (pos.x > limiteX) {
+            pos.x = limiteX;
+        }
+        this.transform.position = pos; //cambia la posicion del objeto
+        */
+
     }
 }
